f23123d8684d8ded2bdd7ff6704b8546f1ccd641
Boxpressd Devs <devs@boxpressd.com>
2026-02-12 09:19:43 -0500

    Upgrade to Next 16

--- next.config.js
+++ next.config.js
@@ -1,23 +1,76 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: [
-      'aficionado.boxpressd.com',
-      'shop.boxpressd.com',
-      'appstore.boxpressd.com',
-      'beta.boxpressd.com',
-      'cdn.cigarsnearme.com',
-      'cdn.boxpressd.com',
-      'www.cigar-register.com',
-      'www.cigarscanner.com',
-      'www.cigarworld.com',
-      'app.lightemupgo.com',
-      'app.cigarsocial.com',
-      'cigarmancave.com',
-      'dojoverse.com',
-      'cigaro.net',
-      'social.cigarpublic.com',
-      'cigar-app.com',
-      'leafenthusiasts.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aficionado.boxpressd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'shop.boxpressd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'appstore.boxpressd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'beta.boxpressd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.cigarsnearme.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.boxpressd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.cigar-register.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.cigarscanner.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.cigarworld.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'app.lightemupgo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'app.cigarsocial.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cigarmancave.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dojoverse.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cigaro.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'social.cigarpublic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cigar-app.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'leafenthusiasts.com',
+      },
     ],
   },
 }
+
